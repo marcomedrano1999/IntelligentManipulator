416de944848cb63db717cbbff563199f3e0f95ef
Marco Medrano <marcomedrano1999@gmail.com>
2022-02-04 17:47:52 -0800

    Update DDPG agent

--- src/DDPG_agent.py
+++ src/DDPG_agent.py
@@ -13,52 +13,130 @@ from tensorflow.keras import layers
 import numpy as np
 import matplotlib as plt
 
+class OUActionNoise:
+    def __init__(self,mean,std_dev,theta=0.15,dt=1e-2,x_initial=None):
+        self.theta=theta
+        self.mean = mean
+        self.std_dev = std_dev
+        self.dt = dt
+        self.x_initial=x_initial
+        self.reset()
+
+    def __call__(self):
+        x=(self.x_prev+self.theta*(self.mean-self.x_prev)*self.dt + self.std_dev*np.sqrt(self.dt)*np.random.normal(size=self.mean.shape))
+        self.x_prev=x
+        return x
+
+    def reset(self):
+        if  self.x_initial is not None:
+            self.x_prev=self.x_initial
+        else:
+            self.x_prev=np.zeros_like(self.mean)
 
 
-class DDPG:
-    """
-    Agent class
-    """
-    def __init__(self, numObs, numActs):
-        """
-        Agent constructor
-        """
-        self.actor = self.create_actor()
+class Buffer:
 
-        self.critic = self.create_critic(numObs,numActs)
+    def __init__(self,num_states, num_actions, buffer_capacity=1000,batch_size=64):
+        # Number of experiences to store at max
+        self.buffer_capacity=buffer_capacity
+        # number of tuples to train on
+        self.batch_size=batch_size
 
-    def create_actor(self, num_states, num_actions):
-        # Actor based in keras
-        
+        # to save the number of time record() was called
+        self.buffer_counter=0
 
-        state_in = layers.Input(shape=(num_states))
-        out = layers.Dense(256, activation="relu")(state_in)
-        out = layers.Dense(256, activation="relu")(out)
-        action_out = layers.Dense(num_actions, activation="tanh")(out)
+        self.state_buffer=np.zeros((self.buffer_capacity,num_states))
+        self.action_buffer=np.zeros((self.buffer_capacity,num_actions))
+        self.reward_buffer = np.zeros((self.buffer_capacity,1))
+        self.next_state_buffer=np.zeros((self.buffer_capacity,num_states))
 
-        tf.keras.Model(state_in,action_out)
+    def record(self, obs_tuple):
+        # Set index to zero if buffer capacity is exceeded
+        index = self.buffer_counter % self.buffer_capacity
 
-    def create_critic(self, num_states,num_actions):
-        # Critic networks are made of two types of inputs: actions and states
+        self.state_buffer[index]=obs_tuple[0]
+        self.action_buffer[index]=obs_tuple[1]
+        self.reward_buffer[index]=obs_tuple[2]
+        self.next_state_buffer[index]=obs_tuple[3]
 
-        # State part
-        state_in = layers.Input(shape=(num_states))
-        state_out = layers.Dense(16, activation="relu")(state_in)
-        state_out = layers.Dense(32, activation="relu")(state_out)
+        self.buffer_counter+=1
 
-        # Action part
-        action_in = layers.Input(shape=(num_actions))
-        action_out = layers.Denser(32,activation="relu")(action_in)
+    @tf.function
+    def update(self, state_batch, action_batch, reward_batch, next_state_batch):
+        global target_actor, target_critic, critic_model, action_model, critic_optimizer, gamma
+        #Training and updating actor and critic networks
+        with tf.GradientTapee() as tape:
+            target_actions = target_actor(next_state_batch,training=True)
+            y = reward_batch+gamma*target_critic([next_state_batch,target_actions], training=True)
+            critic_value = critic_model([state_batch, action_batch], training=True)
+            critic_loss = tf.math.reduce_mean(tf.math.square(y-critic_value))
 
-        # Merge action and critic
-        merged = layers.Concatenate()([state_out, action_out])
+            critic_grad = tape.gradient(critic_loss,critic_model.trainable_variables)
+            critic_optimizer.apply_gradients(zip(critic_grad,critic_model.trainable_variables))
 
-        # Final stages
-        out = layers.Dense(256, activation="relu")(merged)
-        out = layers.Dense(256, activation="relu")(out)
-        output = layers.Dense(1)(out)
+        with tf.GradientTape() as tape:
+            actions = actor_model(state_batch,training=True)
+            critic_value = critic_model([state_batch, actions], training=True)
 
-        critic_model = tf.keras.Model([state_in, action_in], output)
-        
-        return critic_model
+            actor_loss = tf.math.reduce_mean(critic_value)
+
+            actor_grad = tape.gradient(critic_loss, actor_model.trainable_variables)
+            actor_optimizer.apply_gradients(zip(actor_grad,actor_model.trainable_variables))
+    
+    def learn(self):
+        # Get sampling range
+        record_range = min(self.buffer_counter, self.buffer_capacity)
+        # Randomly sampling indices
+        batch_indices = np.random.choice(record_range, self.batch_size)
+
+        # Convert to tensors
+        state_batch = tf.convert_to_tensor(self.state_buffer[batch_indices])
+        action_batch = tf.convert_to_tensor(self.action_buffer[batch_indices])
+        reward_batch = tf.convert_to_tensor(self.reward_buffer[batch_indices])
+        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
+        next_state_batch = tf.convert_to_tensor(self.next_state_buffer[batch_indices])
+
+        self.update(state_batch, action_batch, reward_batch, next_state_batch)
+
+@tf.function
+def update_target(target_weights, weights, tau):
+    for (a,b) in zip(target_weights, weights):
+        a.assign(b*tau+a*(1-tau))
+
+def create_actor(num_states, num_actions):
+    # Actor based in keras
+    
+    state_in = layers.Input(shape=(num_states))
+    out = layers.Dense(256, activation="relu")(state_in)
+    out = layers.Dense(256, activation="relu")(out)
+    action_out = layers.Dense(num_actions, activation="tanh")(out)
+
+    actor_moder = tf.keras.Model(state_in,action_out)
+
+    return actor_moder
+
+
+def create_critic(num_states,num_actions):
+    # Critic networks are made of two types of inputs: actions and states
+
+    # State part
+    state_in = layers.Input(shape=(num_states))
+    state_out = layers.Dense(16, activation="relu")(state_in)
+    state_out = layers.Dense(32, activation="relu")(state_out)
+
+    # Action part
+    action_in = layers.Input(shape=(num_actions))
+    action_out = layers.Dense(32,activation="relu")(action_in)
+
+    # Merge action and critic
+    merged = layers.Concatenate()([state_out, action_out])
+
+    # Final stages
+    out = layers.Dense(256, activation="relu")(merged)
+    out = layers.Dense(256, activation="relu")(out)
+    output = layers.Dense(1)(out)
+
+    critic_model = tf.keras.Model([state_in, action_in], output)
+    
+    return critic_model
 
--- src/Gym_test_env.py
+++ src/Gym_test_env.py
@@ -7,7 +7,12 @@ Date: 01/02/2022
 
 import gym
 import argparse
-from DDPG_agent import DDPG
+import tensorflow as tf
+import numpy as np
+from DDPG_agent import OUActionNoise
+import DDPG_agent
+import matplotlib.pyplot as plt
+
 
 class testEnv:
 
@@ -26,7 +31,7 @@ class testEnv:
         num_actions = 4
 
         # Create DDPG agent
-        agent = DDPG(num_states,num_actions)
+        # agent = DDPG(num_states,num_actions)
 
 
     def run(self, num_episodes=20, num_steps=100):
@@ -49,6 +54,21 @@ class testEnv:
         self.env.close()
 
 
+def policy(state, noise_object):
+    global actor_model
+    #global lower_bound, upper_bound
+
+    sampled_actions = tf.squeeze(actor_model(state))
+    noise = noise_object()
+    # Add noise to the action
+    sampled_actions = sampled_actions.numpy() + noise
+
+    # put action within bounds
+    legal_action = sampled_actions#np.clip(sampled_actions, lower_bound, upper_bound)
+
+    return [np.squeeze(legal_action)]
+
+
 def main():
 
     # Get input parameters
@@ -62,10 +82,94 @@ def main():
     num_steps = args.num_steps
 
     # Create env
-    env = testEnv()
+    env = gym.make('FetchPickAndPlace-v1')
+    env.reset()
 
-    # Train env
-    env.run(num_episodes,num_steps)
+    # Env parameters
+    num_states = 25
+    num_actions = 4
+
+
+    # Noise generator
+    std_dev = 0.2
+    ou_noise = OUActionNoise(mean=np.zeros(1), std_dev=float(std_dev)*np.ones(1))
+
+    actor_model = DDPG_agent.create_actor(num_states,num_actions)
+    critic_model = DDPG_agent.create_critic(num_states,num_actions)
+
+    target_actor = DDPG_agent.create_actor(num_states,num_actions)
+    target_critic = DDPG_agent.create_critic(num_states,num_actions)
+
+
+    # Making the weights equa initially
+    target_actor.set_weights(actor_model.get_weights())
+    target_critic.set_weights(critic_model.get_weights())
+
+    # Learning rate for actor-critic models
+    critic_lr = 0.002
+    actor_lr=0.001
+
+    critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
+    actor_optimizer = tf.keras.optimizers.Adam(actor_lr)
+
+    total_episodes = 100
+    # Discount factor for future rewards
+    gamma = 0.99
+    # value to update networks
+    tau=0.005
+
+    buffer=DDPG_agent.Buffer(num_states, num_actions, 500, 32)
+
+    ep_reward_list = []
+    avg_reward_list = []
+
+    # Model execution
+    for ep in range(total_episodes):
+
+        prev_state = env.reset()["observation"]
+        episodic_reward = 0
+
+        while True:
+            env.render()
+
+            tf_pre_state = tf.expand_dims(tf.convert_to_tensor(prev_state),0)
+
+            action = policy(tf_pre_state, ou_noise)
+            # Recieve state and reward from environment
+            state, reward, done, info = env.step(action)
+
+            state = state[["observation"]]
+
+            buffer.record((prev_state, action, reward, state))
+            episodic_reward +=reward
+            
+            buffer.learn()
+            DDPG_agent.update_target(target_actor.variables, actor_model.variables, tau)
+            DDPG_agent.update_target(target_critic.variables, critic_model.variables, tau)
+
+            if done:
+                break
+
+            prev_state=state
+        
+        ep_reward_list.append(episodic_reward)
+
+        avg_reward = np.mean(ep_reward_list[-40:])
+        print("Episode * {} * Avg reward is : {}".format(ep,avg_reward))
+        avg_reward_list.append(avg_reward)
+    
+    plt.plot(avg_reward_list)
+    plt.xlabel("Episode")
+    plt.ylabel("Avg. Episodic reward")
+    plt.show()
+
+    # Save weights
+    actor_model.save_weights("PickAndPlace_actor.h5")
+    critic_model.save_weights("PickAndPlace_critic.h5")
+
+    target_actor.save_weights("PickAndPlace_target_actor.h5")
+    target_critic.save_weights("PickAndPlace_target_critic.h5")
+
 
 
 if __name__=="__main__":
